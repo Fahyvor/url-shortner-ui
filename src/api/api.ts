961987c7abae8f216af8ff73ad2bbe865d7a7e92
Fahyvor <favourokafor30@gmail.com>
2026-08-27 13:01:13 +0100

    switching backend servers

--- src/api/api.ts
+++ src/api/api.ts
@@ -45,7 +45,7 @@ export interface VideoInfoResponse {
 // AXIOS INSTANCE
 // ==============================
 
-const BASE_URL = "https://oxidlabs.onrender.com";
+const BASE_URL = "https://oxid-labs.onrender.com";
 // const BASE_URL = 'http://localhost:9000';
 
 const api = axios.create({
